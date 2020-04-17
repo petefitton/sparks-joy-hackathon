507fce305ece302e85c160ee9c5c203518326401
Pete Fitton <petefitton@Pete-Fittons-MacBook.local>
2020-04-17 14:04:43 -0700

    Updates

--- profileScript2.js
+++ profileScript2.js
@@ -1,4 +1,3 @@
-console.log("TEST")
 // MODAL
 
 // Get the modal
@@ -24,5 +23,34 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+
+// MODAL #2
+
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("tutorialBtn");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("closer")[0];
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal2
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal2, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
--- profileScript3.js
+++ profileScript3.js
@@ -16,3 +16,39 @@ function scrollUp() {
 function scrollDown() {
     window.scrollBy(0, 100);
 }
+
+function toNext2() {
+    document.getElementById("next1").style.display = "none"
+    document.getElementById("next2").style.display = "block"
+    document.getElementById("tutorial1Img").style.display = "none"
+    document.getElementById("tutorial2Img").style.display = "block"
+}
+
+function toNext3() {
+    document.getElementById("next2").style.display = "none"
+    document.getElementById("next3").style.display = "block"
+    document.getElementById("tutorial2Img").style.display = "none"
+    document.getElementById("tutorial3Img").style.display = "block"
+}
+
+function toNext4() {
+    document.getElementById("next3").style.display = "none"
+    document.getElementById("next4").style.display = "block"
+    document.getElementById("tutorial3Img").style.display = "none"
+    document.getElementById("tutorial4Img").style.display = "block"
+}
+
+function toNext5() {
+    document.getElementById("next4").style.display = "none"
+    document.getElementById("next5").style.display = "block"
+    document.getElementById("tutorial4Img").style.display = "none"
+    document.getElementById("tutorial5Img").style.display = "block"
+}
+
+function toFinish() {
+    // document.getElementById("next2").style.display = "none"
+    // document.getElementById("next3").style.display = "block"
+    // document.getElementById("tutorial2Img").style.display = "none"
+    // document.getElementById("tutorial3Img").style.display = "block"
+    document.getElementById("myModal2").style.display = "none"
+}
